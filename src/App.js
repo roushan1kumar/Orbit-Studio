63a1de0d1a521f8a35eaea33e616b1fc581aca1f
Roushan Kumar <roushan1kumar9205@gmail.com>
2026-06-05 16:59:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,84 +3,6 @@ import Navbar from "./components/Navbar";
 import Scene from "./components/Scene";
 import BackgroundAnimation from "./components/BackgroundAnimation";
 import Contact from "./components/Contact";
-<<<<<<< HEAD
-import PageMotion from "./components/PageMotion";
-import StarBackground from "./components/StarBackground";
-
-function App() {
-
-  return (
-
-    <div className="relative text-white overflow-x-hidden min-h-screen">
-
-      {/* Animated Background */}
-      <BackgroundAnimation />
-
-      {/* Navbar */}
-      <Navbar />
-
-      {/* HOME SECTION */}
-      <section
-        id="home"
-        className="min-h-screen flex items-center justify-center transition duration-500 hover:scale-[1.02]"
-      >
-
-        {/* 3D Background */}
-        <div className="absolute inset-0 opacity-60">
-          <Scene />
-        </div>
-
-        {/* Hero Content */}
-        <PageMotion>
-          <div className="relative z-10">
-
-            <h1 className="text-7xl md:text-9xl font-bold tracking-wide">
-              Flux Studio
-            </h1>
-
-            <p className="mt-6 text-gray-300 text-lg max-w-2xl mx-auto">
-              Creative Developer & Motion Designer
-            </p>
-
-          </div>
-        </PageMotion>
-
-      </section>
-
-      {/* PROJECTS */}
-      <section
-        id="projects"
-        className="min-h-screen flex items-center justify-center transition duration-500 hover:scale-[1.02]"
-      >
-
-        <h1 className="text-6xl font-bold">
-          Projects Section
-        </h1>
-
-      </section>
-
-      <div className="relative text-white overflow-x-hidden min-h-screen">
-
-        <StarBackground />
-
-        <Navbar />
-
-      </div>
-
-      {/* ABOUT */}
-      <section
-        id="about"
-        className="min-h-screen flex items-center justify-center transition duration-500 hover:scale-[1.02]"
-      >
-
-        <h1 className="text-6xl font-bold">
-          About Section
-        </h1>
-
-      </section>
-
-      {/* CONTACT COMPONENT */}
-=======
 import StarBackground from "./components/StarBackground";
 import WorkShowcase3D from "./components/WorkShowcase3D";
 import AboutStudio from "./components/AboutStudio";
@@ -790,7 +712,6 @@ function App() {
       </section>
 
       {/* CONTACT */}
->>>>>>> 6d730ce (first commit)
       <Contact />
 
     </div>
